521ea1e6ea362c6c7f784e584f2033205fccc051
osha7 <ogroetz@gmail.com>
2020-05-16 23:10:13 -0600

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        #lists the characters of actor with shows character in
+        self.characters.collect do |character| 
+            "#{character.name} - #{Show.find(character.show_id).name}"
+        end
+    end
   
 end
