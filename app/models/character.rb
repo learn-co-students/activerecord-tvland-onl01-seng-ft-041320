a47dec535df499101b132ec599183802a11b0be0
ReginaF2012 <skritschy@gmail.com>
2020-05-17 18:41:46 -0500

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,27 @@
 class Character < ActiveRecord::Base
+  belongs_to :actor 
+  belongs_to :show
   
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+
+  def build_show(attr)
+    #show = Show.new(attr)
+    #show.characters << self
+    #self
+    self.tap do |s|
+      s.show = Show.new(attr)
+      s.show.characters << self
+    end
+  end
+  
+
+  def build_network(args)
+    self.tap do |s|
+      s.show.network = Network.new(args)
+    end
+  end
+
+
 end
