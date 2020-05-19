59646d2fb6fd93d8a351ea8eb85d34e158e7c45a
github username <github email address>
2020-05-19 15:16:03 -0400

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,23 @@
 class Character < ActiveRecord::Base
+    belongs_to :show
+    belongs_to :actor 
   
+    def say_that_thing_you_say
+        "#{self.name} always says: #{self.catchphrase}"
+      end
+    
+      def build_show(attr)
+        self.tap do |s|
+          s.show = Show.new(attr)
+          s.show.characters << self
+        end
+      end
+      
+    
+      def build_network(args)
+        self.tap do |s|
+          s.show.network = Network.new(args)
+        end
+      end
+
 end
