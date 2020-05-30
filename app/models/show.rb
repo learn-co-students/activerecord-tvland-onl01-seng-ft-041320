5aea797090b55ebed462fa5ae2dcbe860d3dd53a
dbhmunro <dbhmunro@gmail.com>
2020-05-30 15:55:45 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :actor
+    belongs_to :network
+    has_many :characters
+
+    def actors_list
+        self.characters.map do |c|
+            "#{c.actor.full_name}"
+        end
+    end
 end
